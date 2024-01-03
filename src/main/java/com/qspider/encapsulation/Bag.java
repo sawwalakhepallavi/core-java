1c3a13cbbc4f9882f7a1f945a001449d2c494553
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2024-01-04 00:03:16 +0530

    ...

---
+++ src/main/java/com/qspider/encapsulation/Bag.java
@@ -0,0 +1,22 @@
+package com.qspider.encapsulation;
+
+public class Bag {
+    private String Color;
+    private int price;
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
